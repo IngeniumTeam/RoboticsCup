ff03759d2b08ead17389f61acc898dd881ad19b4
SimonPucheu <104694344+SimonPucheu@users.noreply.github.com>
2023-09-25 19:31:13 +0200

    Update

--- admin/script.js
+++ admin/script.js
@@ -1,5 +1,4 @@
 async function getCurrentFileContent(owner, repo, filePath, accessToken) {
-    console.log(filePath);
     const response = await fetch(`https://api.github.com/repos/${owner}/${repo}/contents/${filePath}`, {
         method: 'GET',
         headers: {
@@ -16,10 +15,29 @@ async function getCurrentFileContent(owner, repo, filePath, accessToken) {
     }
 }
 
-async function updateFileContent(owner, repo, filePath, accessToken, data, date, place, content) {
+async function updateFileContent(owner, repo, filePath, accessToken, data, lang, date, place, content) {
     const currentContent = atob(data.content);
-    const newContent = currentContent + '\nThis is the updated content';
-    const encodedContent = btoa(newContent);
+    const fileData = await getCurrentFileContent(owner, repo, filePath, accessToken);
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(currentContent, 'text/html');
+    const eventsContainer = doc.querySelector('.events');
+    const newEventDiv = document.createElement('div');
+    newEventDiv.classList.add('event');
+    newEventDiv.innerHTML = `
+    <input type="date" value="${date}" readonly class="date">
+    <span class="place">${place}</span>
+    <span class="content">${content}</span>
+  `;
+    const existingEvent = eventsContainer.querySelector(`.event input.date[value="${date}"]`);
+    if (existingEvent) {
+        existingEvent.closest('.event').replaceWith(newEventDiv);
+    } else {
+        eventsContainer.appendChild(newEventDiv);
+    }
+    const updatedContent = new XMLSerializer().serializeToString(doc);
+    console.log(updatedContent);
+    const encodedContent = btoa(updatedContent.replace('Â', ''));
+    await updateFileContent(owner, repo, filePath, accessToken, fileData, 'HTML Update', date, place, encodedContent);
     const commitMessage = `Update ${lang}/index.html ${date}`;
     const response = await fetch(`https://api.github.com/repos/${owner}/${repo}/contents/${filePath}`, {
         method: 'PUT',
@@ -45,15 +63,14 @@ document.querySelector('form').onsubmit = async function () {
     var fd = new FormData(this);
     var ACCESS_TOKEN;
     try {
-        ACCESS_TOKEN = await fetch(`https://simonpucheu.000webhostapp.com/IngeniumTeam/github_token.php?key=${fd.get('password')}`);
+        ACCESS_TOKEN = await (await fetch(`https://simonpucheu.000webhostapp.com/IngeniumTeam/github_token.php?key=${fd.get('password')}`)).text();
     } catch (error) {
         alert(error.message);
         return;
     }
-    console.log(ACCESS_TOKEN);
     try {
         const fileData = await getCurrentFileContent('IngeniumTeam', 'ingeniumteam.github.io', fd.get('lang') + '/index.html', ACCESS_TOKEN);
-        await updateFileContent('IngeniumTeam', 'ingeniumteam.github.io', fd.get('lang'), ACCESS_TOKEN, fileData, fd.get('date'), fd.get('place'), fd.get('content'));
+        await updateFileContent('IngeniumTeam', 'ingeniumteam.github.io', fd.get('lang') + '/index.html', ACCESS_TOKEN, fileData, fd.get('lang'), fd.get('date'), fd.get('place'), fd.get('content'));
     } catch (error) {
         alert(error.message);
     }
